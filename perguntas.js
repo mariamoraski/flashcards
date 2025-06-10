e344536829cbb41b52c02fc329671d44efd63ebe
mariamoraski <maria.moraski@escola.pr.gov.br>
2025-06-10 22:38:11 +0000

    aula 6

---
+++ perguntas.js
@@ -0,0 +1,37 @@
+
+criaCartao(
+    'Stranger Things',
+    'Quem é o criador da série Stranger Things?',
+    'Os irmãos Duffer (Matt e Ross Duffer).'
+)
+
+criaCartao(
+    'Stranger Things',
+    'Em que cidade fictícia se passa a maior parte da série?',
+    'Hawkins, Indiana.'
+)
+
+criaCartao(
+    'Stranger Things',
+    'Qual o nome do monstro da primeira temporada?',
+    'Demogorgon.'
+)
+
+criaCartao(
+    'Stranger things',
+    'O que é o Mundo Invertido (Upside Down)?',
+    'Uma dimensão paralela sombria e perigosa, que espelha o mundo real.'
+)
+
+criaCartao(
+    'Stranger Things',
+    'Qual o nome da garota com poderes psíquicos que foge do laboratório?',
+    'Eleven (Onze).'
+)
+
+criaCartao(
+    'Stranger Things',
+    'Qual o nome da mãe de Will, que usa luzes de Natal para se comunicar com ele?',
+    'Joyce Byers..'
+)
+
