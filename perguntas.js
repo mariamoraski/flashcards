e085e082654d5dbdd0fb691573468271ea774230
mariamoraski <maria.moraski@escola.pr.gov.br>
2025-07-03 19:28:25 +0000

    js atualizado

--- perguntas.js
+++ perguntas.js
@@ -1,37 +1,54 @@
 
 criaCartao(
-    'Stranger Things',
-    'Quem é o criador da série Stranger Things?',
-    'Os irmãos Duffer (Matt e Ross Duffer).'
+    'CEP',
+    'Piscina olímpica',
+    'A piscina olímpica do CEP foi inaugurada no início da década de 1950 e é uma das mais antigas do estado do Paraná. Ainda é utilizada pelos alunos até hoje  .'
 )
 
 criaCartao(
-    'Stranger Things',
-    'Em que cidade fictícia se passa a maior parte da série?',
-    'Hawkins, Indiana.'
+    'CEP',
+    'Ano de fundação',
+    'A instituição teve sua origem em 1846, como Liceu de Coritiba. Em 1943 passou a se chamar Colégio Estadual do Paraná, já funcionando no local atual desde 1950  .'
 )
 
 criaCartao(
-    'Stranger Things',
-    'Qual o nome do monstro da primeira temporada?',
-    'Demogorgon.'
+    'CEP',
+    'Tombamento',
+    'O CEP foi tombado como patrimônio histórico do estado em 1994, após solicitação de três ex-alunas  .'
 )
 
 criaCartao(
-    'Stranger things',
-    'O que é o Mundo Invertido (Upside Down)?',
-    'Uma dimensão paralela sombria e perigosa, que espelha o mundo real.'
+    'CEP',
+    'Túnel(s) no colégio',
+    'O colégio possui um abrigo anti-aéreo, construído na época da inauguração, que hoje é usado pela Escolinha de Arte. Não há registros específicos de “túneis”, mas este abrigo pode ser a estrutura referida  .'
 )
 
 criaCartao(
-    'Stranger Things',
-    'Qual o nome da garota com poderes psíquicos que foge do laboratório?',
-    'Eleven (Onze).'
+    'CEP',
+    'Inauguração do planetário',
+    'O Planetário do CEP foi inaugurado em 27 de abril de 1978, com capacidade para aproximadamente 62–63 espectadores, cúpula de 6 m e projetor Zeiss ZKP‑1  ..'
 )
 
 criaCartao(
-    'Stranger Things',
-    'Qual o nome da mãe de Will, que usa luzes de Natal para se comunicar com ele?',
-    'Joyce Byers..'
+    'CEP',
+    'Tamanho do terreno',
+    'A sede ocupa um terreno de 40.000 m², conforme documentos da Secretaria de Educação  .'
 )
 
+criaCartao(
+    'CEP',
+    'Motivo da construção',
+    'O colégio foi fundado em meados do século XIX para atender à educação pública em Curitiba. A sede atual (iniciada em 1944, inaugurada em 1950) foi construída para abrigar o antigo Gymnasio Paranaense em uma estrutura moderna e ampliar o atendimento ao estudante do estado  .'
+)
+
+criaCartao(
+    'CEP',
+    'Esportes praticados',
+    'O CEP possui ampla infraestrutura esportiva: duas piscinas (inclusive uma olímpica), ginásio, quadras (futebol, basquete, vôlei), pista de atletismo e campo de futebol. Embora o número exato de modalidades não seja citado, essas instalações permitem a prática de pelo menos 5–6 esportes  .'
+)
+
+criaCartao(
+    'CEP',
+    'Quantidade média de alunos',
+    'Estima-se que, anualmente, cerca de 5.200 alunos estudam no CEP'
+)
